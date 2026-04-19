35afcdf69e49413e13ecdfc9e503332343fea7ca
kamilkubrak <kamil.kubrak@studenci.collegiumwitelona.pl>
2026-04-19 15:35:34 +0200

    Revert "harmonogram"
    This reverts commit 0f6f297771d285204d29e1c664fca7258233d8ec.

--- GymAdminPanel/GymAdminPanel/Converters/Converters.cs
+++ GymAdminPanel/GymAdminPanel/Converters/Converters.cs
@@ -11,40 +11,21 @@ public class RoleToColorConverter : IValueConverter
     {
         return value?.ToString() switch
         {
-            "ROLE_ADMIN" => new SolidColorBrush(Color.FromRgb(39, 174, 96)),
-            "ROLE_TRAINER" => new SolidColorBrush(Color.FromRgb(142, 68, 173)),
-            "ROLE_USER" => new SolidColorBrush(Color.FromRgb(41, 128, 185)),
-            _ => new SolidColorBrush(Color.FromRgb(127, 140, 141)),
+            "ROLE_ADMIN" => new SolidColorBrush(Color.FromRgb(39, 174, 96)),   // zielony
+            "ROLE_TRAINER" => new SolidColorBrush(Color.FromRgb(142, 68, 173)),  // fioletowy
+            "ROLE_USER" => new SolidColorBrush(Color.FromRgb(41, 128, 185)),  // niebieski
+            _ => new SolidColorBrush(Color.FromRgb(127, 140, 141)), // szary
         };
     }
+
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         => DependencyProperty.UnsetValue;
 }
-
 public class BoolToVisibilityConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         => value is true ? Visibility.Visible : Visibility.Collapsed;
+
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         => value is Visibility.Visible;
-}
-
-public class BoolToRedGreenConverter : IValueConverter
-{
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        => value is true
-            ? new SolidColorBrush(Color.FromRgb(231, 76, 60))
-            : new SolidColorBrush(Color.FromRgb(39, 174, 96));
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        => DependencyProperty.UnsetValue;
-}
-
-public class BoolToTypeColorConverter : IValueConverter
-{
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        => value is true
-            ? new SolidColorBrush(Color.FromRgb(142, 68, 173))
-            : new SolidColorBrush(Color.FromRgb(41, 128, 185));
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        => DependencyProperty.UnsetValue;
 }
